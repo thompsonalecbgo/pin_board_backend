240fbd376a84edee046154745dda5e4ac33019fb
Thompson Go <thompsongo2020@gmail.com>
2020-12-09 07:50:31 +0800

    preparing to add new model for links

--- notes/models.py
+++ notes/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Note(models.Model):
     text = models.TextField(blank=True)
-    top = models.PositiveIntegerField(default=0)
-    left = models.PositiveIntegerField(default=0)
+    top = models.IntegerField(default=0)
+    left = models.IntegerField(default=0)
 
     def __str__(self):
         return f"{self.text[:100]}"
