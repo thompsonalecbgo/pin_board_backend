99eaf6f7c6f2502c51f2218464069c6e53a55b76
Thompson Go <thompsongo2020@gmail.com>
2020-12-11 22:26:54 +0800

    preparing to deploy

--- backend/settings.py
+++ backend/settings.py
@@ -135,7 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = BASE_DIR / '/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static',] #
 
 AUTH_USER_MODEL = "users.User"
 
