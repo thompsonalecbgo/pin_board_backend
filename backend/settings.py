0b547f1aedf2696c8f333ee37474e6c0ee927cbd
Thompson Go <thompsongo2020@gmail.com>
2020-12-11 22:24:02 +0800

    updated static url

--- backend/settings.py
+++ backend/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = BASE_DIR / '/static/'
 
 AUTH_USER_MODEL = "users.User"
 
